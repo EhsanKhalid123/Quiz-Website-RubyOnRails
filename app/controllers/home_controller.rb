7119f8d146b2bb85f5935af5effde4b16ef7e1c2
Ehsan Khalid <s3838975@student.rmit.edu.au>
2021-06-17 03:38:47 +0000

    Displays Questions and Options from JSON data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+
+    require "json"
+
+   file = File.read('quiz.json')
+   data = JSON.parse(file)
+    @data = data.sample
+
+
+
   end
 end
